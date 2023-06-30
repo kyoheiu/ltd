b185050bf4ec1f9306d9918d8a3944c680d42357
Kyohei Uto <im@kyoheiu.dev>
2023-07-01 06:57:44 +0900

    Add style to Login

--- svelte/src/lib/Login.svelte
+++ svelte/src/lib/Login.svelte
@@ -1,33 +1,39 @@
-<div class="text-3xl text-slate-200 text-center m-2 mt-4">ltd</div>
-<div>
-    <form
-        class="space-y-2 m-4 text-center"
-        id="login-form"
-        method="post"
-        action="api/ldaplogin"
-    >
-        <div>
-            <input
-                class="rounded-md p-1"
-                type="text"
-                name="username"
-                placeholder="DN"
-                required
-            />
-        </div>
-        <div>
-            <input
-                class="rounded-md p-1"
-                type="password"
-                name="password"
-                placeholder="PASSWORD"
-                required
-            />
-        </div>
-        <p>
-            <button class="text-2xl text-slate-200 m-2" type="submit"
-                ><i class="ri-login-circle-line" /></button
-            >
-        </p>
-    </form>
+<script>
+    import logo from "../assets/logo.png";
+</script>
+
+<div class="flex flex-col items-center mt-4 p-6">
+    <img src={logo} alt="ltd" class="w-8 h-auto mb-2" />
+    <div>
+        <form
+            class="space-y-2 m-4 text-center"
+            id="login-form"
+            method="post"
+            action="api/ldaplogin"
+        >
+            <div>
+                <input
+                    class="rounded-md bg-slate-200 p-1"
+                    type="text"
+                    name="username"
+                    placeholder="DN"
+                    required
+                />
+            </div>
+            <div>
+                <input
+                    class="rounded-md bg-slate-200 p-1"
+                    type="password"
+                    name="password"
+                    placeholder="PASSWORD"
+                    required
+                />
+            </div>
+            <p>
+                <button class="text-2xl text-slate-200 m-2" type="submit"
+                    ><i class="ri-login-circle-line" /></button
+                >
+            </p>
+        </form>
+    </div>
 </div>
