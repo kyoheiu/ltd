58e8bc94e7411b5860e582733ede4762116af61b
Kyohei Uto <im@kyoheiu.dev>
2023-07-01 15:31:31 +0900

    Display current value

--- svelte/src/lib/Rename.svelte
+++ svelte/src/lib/Rename.svelte
@@ -4,8 +4,9 @@
   export let id;
 </script>
 
+<div class="text-slate-200 text-xl p-2">{value}</div>
 <form class="flex flex-col justify-items-center content-center space-y-2 m-1 rounded-md" action="/api/rename" method="post">
-  <input class="p-1 rounded-md" type="text" name="value" value={value} />
+  <input class="bg-slate-200 p-1 rounded-md" type="text" name="value" value={value} />
   <input type="hidden" name="id" value={id} />
   <button class="text-sm border-2 bg-slate-200 w-min px-1 rounded-md ml-auto"><i class="ri-loop-right-line"></i></button>
 </form>
