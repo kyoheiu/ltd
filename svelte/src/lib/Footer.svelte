5966e62f2cfd99649046171e5d46bd64642e76a9
Kyohei Uto <im@kyoheiu.dev>
2023-07-08 07:09:50 +0900

    Add GitHub link

--- svelte/src/lib/Footer.svelte
+++ svelte/src/lib/Footer.svelte
@@ -1,1 +1,5 @@
-<footer class="text-sm p-4">ltd v0.4.1 <a href="https://github.com/kyoheiu/ltd"><i class="ri-github-fill" /></a></footer>
+<footer class="text-sm p-4">
+  ltd v0.4.1 <a href="https://github.com/kyoheiu/ltd"
+    ><i class="ri-github-fill" /></a
+  >
+</footer>
