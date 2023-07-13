9564a79e9eb2a24df3c7268afa125c4a0df78e97
Kyohei Uto <im@kyoheiu.dev>
2023-07-13 16:20:06 +0900

    Fix footer

--- svelte/src/lib/Footer.svelte
+++ svelte/src/lib/Footer.svelte
@@ -1,5 +1,5 @@
-<footer class="text-sm p-4">
-  ltd v0.4.1 <a href="https://github.com/kyoheiu/ltd"
-    ><i class="ri-github-fill" /></a
+<footer class="text-sm m-4 hover:underline">
+  <a href="https://github.com/kyoheiu/ltd"
+    >ltd v0.4.1</a
   >
 </footer>
