ffcd93b9e51a9ddce14e4861bdcde8671aceca18
Kyohei Uto <im@kyoheiu.dev>
2023-08-21 17:22:54 +0900

    Change onMount for demo

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -4,7 +4,7 @@
   import Login from "./lib/Login.svelte";
   import { Page } from "./lib/types.ts";
   import { SvelteToast } from "@zerodevx/svelte-toast";
-  import { readItem, state } from "./lib/stores.ts";
+  import { State, readItem, state } from "./lib/stores.ts";
   import Header from "./lib/Header.svelte";
   import All from "./lib/All.svelte";
   import ItemColored from "./lib/ItemColored.svelte";
@@ -21,58 +21,58 @@
     intro: { y: 40 },
   };
 
-  onMount(async () => {
-    readItem();
-  });
+  // onMount(async () => {
+  //   readItem();
+  // });
 
   // for demo
-  // onMount(() => {
-  //   const items = [
-  //     {
-  //       id: "1",
-  //       value: "milk",
-  //       todo: true,
-  //       dot: 0,
-  //       showModal: false,
-  //     },
-  //     {
-  //       id: "2",
-  //       value: "orange",
-  //       todo: true,
-  //       dot: 0,
-  //       showModal: false,
-  //     },
-  //     {
-  //       id: "3",
-  //       value: "banana",
-  //       todo: true,
-  //       dot: 0,
-  //       showModal: false,
-  //     },
-  //     {
-  //       id: "4",
-  //       value: "apple",
-  //       todo: true,
-  //       dot: 0,
-  //       showModal: false,
-  //     },
-  //     {
-  //       id: "5",
-  //       value: "watermelon",
-  //       todo: true,
-  //       dot: 0,
-  //       showModal: false,
-  //     },
-  //   ];
+  onMount(() => {
+    const items = [
+      {
+        id: "1",
+        value: "milk",
+        todo: true,
+        dot: 0,
+        showModal: false,
+      },
+      {
+        id: "2",
+        value: "orange",
+        todo: true,
+        dot: 0,
+        showModal: false,
+      },
+      {
+        id: "3",
+        value: "banana",
+        todo: true,
+        dot: 0,
+        showModal: false,
+      },
+      {
+        id: "4",
+        value: "apple",
+        todo: true,
+        dot: 0,
+        showModal: false,
+      },
+      {
+        id: "5",
+        value: "watermelon",
+        todo: true,
+        dot: 0,
+        showModal: false,
+      },
+    ];
 
-  //   state.update((s) => {
-  //     return {
-  //       ...new State(),
-  //       page: Page.All,
-  //       items: items,
-  //     };
-  //   });
-  // });
+    state.update((s) => {
+      return {
+        ...new State(),
+        page: Page.All,
+        items: items,
+      };
+    });
+  });
 </script>
 
 <SvelteToast {options} />
