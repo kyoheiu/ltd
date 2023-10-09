6102f5640b43708186b51fadd147b89f0da30070
Kyohei Uto <im@kyoheiu.dev>
2023-10-09 17:50:50 +0900

    Disable data loader & fetching for demo

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -4,64 +4,64 @@
   import { onMount } from "svelte";
   import Login from "./lib/Login.svelte";
   import { Page } from "./lib/types.ts";
-  import { readItem, state } from "./lib/stores.ts";
+  import { State, readItem, state } from "./lib/stores.ts";
   import Header from "./lib/Header.svelte";
   import All from "./lib/All.svelte";
   import ItemColored from "./lib/ItemColored.svelte";
   import Archived from "./lib/Archived.svelte";
 
-  onMount(async () => {
-    readItem();
-  });
+  // onMount(async () => {
+  //   readItem();
+  // });
 
   // for demo
-  // onMount(() => {
-  //   const items = [
-  //     {
-  //       id: "1",
-  //       value: "milk",
-  //       todo: true,
-  //       dot: 0,
-  //       showModal: false,
-  //     },
-  //     {
-  //       id: "2",
-  //       value: "orange",
-  //       todo: true,
-  //       dot: 0,
-  //       showModal: false,
-  //     },
-  //     {
-  //       id: "3",
-  //       value: "banana",
-  //       todo: true,
-  //       dot: 0,
-  //       showModal: false,
-  //     },
-  //     {
-  //       id: "4",
-  //       value: "apple",
-  //       todo: true,
-  //       dot: 0,
-  //       showModal: false,
-  //     },
-  //     {
-  //       id: "5",
-  //       value: "watermelon",
-  //       todo: true,
-  //       dot: 0,
-  //       showModal: false,
-  //     },
-  //   ];
+  onMount(() => {
+    const items = [
+      {
+        id: "1",
+        value: "milk",
+        todo: true,
+        dot: 0,
+        showModal: false,
+      },
+      {
+        id: "2",
+        value: "orange",
+        todo: true,
+        dot: 1,
+        showModal: false,
+      },
+      {
+        id: "3",
+        value: "banana",
+        todo: true,
+        dot: 2,
+        showModal: false,
+      },
+      {
+        id: "4",
+        value: "apple",
+        todo: true,
+        dot: 3,
+        showModal: false,
+      },
+      {
+        id: "5",
+        value: "watermelon",
+        todo: true,
+        dot: 0,
+        showModal: false,
+      },
+    ];
 
-  //   state.update((s) => {
-  //     return {
-  //       ...new State(),
-  //       page: Page.All,
-  //       items: items,
-  //     };
-  //   });
-  // });
+    state.update((s) => {
+      return {
+        ...new State(),
+        page: Page.All,
+        items: items,
+      };
+    });
+  });
 </script>
 
 <div class="wrap" />
--- svelte/src/lib/toggle.ts
+++ svelte/src/lib/toggle.ts
@@ -26,29 +26,29 @@ export const toggleArchived = async (id: string) => {
         archived: archived,
       };
     });
-    const res = await fetch(`/api/item?toggle_todo=1&id=${id}`, {
-      method: "POST",
-    });
-    if (!res.ok) {
-      archived.splice(i, 0, target);
-      items.shift();
-      state.update((s) => {
-        return {
-          ...s,
-          items: items,
-          archived: archived,
-        };
-      });
-      toastError(res.statusText);
-      return;
-    }
-    const j = await res.json();
-    state.update((s) => {
-      return {
-        ...s,
-        modified: j.modified,
-      };
-    });
+    // const res = await fetch(`/api/item?toggle_todo=1&id=${id}`, {
+    //   method: "POST",
+    // });
+    // if (!res.ok) {
+    //   archived.splice(i, 0, target);
+    //   items.shift();
+    //   state.update((s) => {
+    //     return {
+    //       ...s,
+    //       items: items,
+    //       archived: archived,
+    //     };
+    //   });
+    //   toastError(res.statusText);
+    //   return;
+    // }
+    // const j = await res.json();
+    // state.update((s) => {
+    //   return {
+    //     ...s,
+    //     modified: j.modified,
+    //   };
+    // });
   } else {
     i = items.findIndex((item) => item.id == id);
     const target2 = items[i];
@@ -64,59 +64,59 @@ export const toggleArchived = async (id: string) => {
     });
 
     // If the toast is clicked before dismissed, archiving will be cancelled.
-    var aborted = false;
-    var t = Toastify({
-      text: `Archived ${target2.value}. &nbsp;<u>Cancel</u>`,
-      escapeMarkup: false,
-      className:
-        "!border-l-8 !border-ok !bg-none !bg-foreground !text-none !text-background",
-      duration: 2000,
-      gravity: "bottom",
-      position: "center",
-      stopOnFocus: true,
-      onClick: () => {
-        items.splice(i, 0, target2);
-        archived.shift();
-        state.update((s) => {
-          return {
-            ...s,
-            items: items,
-            archived: archived,
-          };
-        });
-        aborted = true;
-        t.hideToast();
-      },
-    });
-    t.showToast();
-    await sleep(2000);
-    if (aborted) {
-      return;
-    }
+    // var aborted = false;
+    // var t = Toastify({
+    //   text: `Archived ${target2.value}. &nbsp;<u>Cancel</u>`,
+    //   escapeMarkup: false,
+    //   className:
+    //     "!border-l-8 !border-ok !bg-none !bg-foreground !text-none !text-background",
+    //   duration: 2000,
+    //   gravity: "bottom",
+    //   position: "center",
+    //   stopOnFocus: true,
+    //   onClick: () => {
+    //     items.splice(i, 0, target2);
+    //     archived.shift();
+    //     state.update((s) => {
+    //       return {
+    //         ...s,
+    //         items: items,
+    //         archived: archived,
+    //       };
+    //     });
+    //     aborted = true;
+    //     t.hideToast();
+    //   },
+    // });
+    // t.showToast();
+    // await sleep(2000);
+    // if (aborted) {
+    //   return;
+    // }
 
-    const res = await fetch(`/api/item?toggle_todo=0&id=${id}`, {
-      method: "POST",
-    });
-    if (!res.ok) {
-      items.splice(i, 0, target2);
-      archived.shift();
-      state.update((s) => {
-        return {
-          ...s,
-          items: items,
-          archived: archived,
-        };
-      });
-      toastError(res.statusText);
-      return;
-    }
-    const j = await res.json();
-    state.update((s) => {
-      return {
-        ...s,
-        modified: j.modified,
-      };
-    });
+    // const res = await fetch(`/api/item?toggle_todo=0&id=${id}`, {
+    //   method: "POST",
+    // });
+    // if (!res.ok) {
+    //   items.splice(i, 0, target2);
+    //   archived.shift();
+    //   state.update((s) => {
+    //     return {
+    //       ...s,
+    //       items: items,
+    //       archived: archived,
+    //     };
+    //   });
+    //   toastError(res.statusText);
+    //   return;
+    // }
+    // const j = await res.json();
+    // state.update((s) => {
+    //   return {
+    //     ...s,
+    //     modified: j.modified,
+    //   };
+    // });
   }
 };
 
@@ -139,35 +139,35 @@ export const changeColor = async (id: string) => {
       };
     });
 
-    const res = await fetch(
-      `/api/item?toggle_dot=${archived[i].dot}&id=${id}`,
-      {
-        method: "POST",
-      }
-    );
-    if (!res.ok) {
-      if (archived[i].dot === 0) {
-        archived[i].dot = 3;
-      } else {
-        archived[i].dot -= 1;
-      }
-      state.update((s) => {
-        return {
-          ...s,
-          archived: archived,
-        };
-      });
-      toastError(res.statusText);
-      return;
-    }
-    const j = await res.json();
+    // const res = await fetch(
+    //   `/api/item?toggle_dot=${archived[i].dot}&id=${id}`,
+    //   {
+    //     method: "POST",
+    //   }
+    // );
+    // if (!res.ok) {
+    //   if (archived[i].dot === 0) {
+    //     archived[i].dot = 3;
+    //   } else {
+    //     archived[i].dot -= 1;
+    //   }
+    //   state.update((s) => {
+    //     return {
+    //       ...s,
+    //       archived: archived,
+    //     };
+    //   });
+    //   toastError(res.statusText);
+    //   return;
+    // }
+    // const j = await res.json();
 
-    state.update((s) => {
-      return {
-        ...s,
-        modified: j.modified,
-      };
-    });
+    // state.update((s) => {
+    //   return {
+    //     ...s,
+    //     modified: j.modified,
+    //   };
+    // });
   } else {
     let items = currentState.items;
     i = items.findIndex((item) => item.id === id);
@@ -184,31 +184,31 @@ export const changeColor = async (id: string) => {
       };
     });
 
-    const res = await fetch(`/api/item?toggle_dot=${items[i].dot}&id=${id}`, {
-      method: "POST",
-    });
-    if (!res.ok) {
-      if (items[i].dot === 0) {
-        items[i].dot = 3;
-      } else {
-        items[i].dot -= 1;
-      }
-      state.update((s) => {
-        return {
-          ...s,
-          items: items,
-        };
-      });
-      toastError(res.statusText);
-      return;
-    }
-    const j = await res.json();
+    // const res = await fetch(`/api/item?toggle_dot=${items[i].dot}&id=${id}`, {
+    //   method: "POST",
+    // });
+    // if (!res.ok) {
+    //   if (items[i].dot === 0) {
+    //     items[i].dot = 3;
+    //   } else {
+    //     items[i].dot -= 1;
+    //   }
+    //   state.update((s) => {
+    //     return {
+    //       ...s,
+    //       items: items,
+    //     };
+    //   });
+    //   toastError(res.statusText);
+    //   return;
+    // }
+    // const j = await res.json();
 
-    state.update((s) => {
-      return {
-        ...s,
-        modified: j.modified,
-      };
-    });
+    // state.update((s) => {
+    //   return {
+    //     ...s,
+    //     modified: j.modified,
+    //   };
+    // });
   }
 };
