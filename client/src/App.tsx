5144503c96e0dc59d2a4824db9db311185e08786
Kyohei Uto <im@kyoheiu.dev>
2025-12-30 07:15:55 +0900

    Add react files

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import logo from "./assets/logo.png";
+
+function App() {
+  return (
+    <div>
+      <img src={logo} style={{ width: "20px" }} />
+      <div>test</div>
+    </div>
+  );
+}
+
+export default App;
